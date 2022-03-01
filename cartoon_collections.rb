f6e2404a47c7e0117286d24728f0fda94ce3f129
mborreros <mlbsindia@gmail.com>
2022-02-28 19:11:52 -0500

    commit tree d0a1f01884574ea87a7b8a0fb1cc813ebeafa3b2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves(array)
+  array.each_with_index.map {|dwarf, index| puts "#{index+1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  capitalized_array = []
+  array.map {|call| 
+  capitalized_call = "#{call.capitalize}!"
+  capitalized_array << capitalized_call
+  } 
+  capitalized_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|call| call.length>4} 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  matching_cheese = []
+  for cheese in cheese_types do
+    for item in array do
+      if item == cheese
+        matching_cheese << cheese
+      end
+    end
+  end
+  if matching_cheese.empty?
+    nil
+  else
+    matching_cheese.join
+  end
 end
